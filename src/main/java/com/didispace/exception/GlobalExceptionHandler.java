be31ca41b3ae6a633f9dec17578b17a9480d8d2a
Arison <784602719@qq.com>
2019-01-09 15:07:54 +0800

    commit

--- src/main/java/com/didispace/exception/GlobalExceptionHandler.java
+++ src/main/java/com/didispace/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
-@SuppressWarnings("unused")
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
